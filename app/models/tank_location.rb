7456e8e7f6991206c56f9df3d4fc8eabff8f87a1
Joshua Alletto <joshalletto@gmail.com>
2018-04-16 16:12:02 -0500

    add geocoder and gmap4rails gems

--- app/models/tank_location.rb
+++ app/models/tank_location.rb
@@ -1,4 +1,8 @@
 class TankLocation < ApplicationRecord
+  validates :address, presence: true
+
+  geocode_by :address
+
   has_many :comments, as: :commentable
   has_many :photos
   belongs_to :user
