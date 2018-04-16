c1ae7fa92af965476e131c239111e2c9a2c8abe8
Joshua Alletto <joshalletto@gmail.com>
2018-04-16 11:00:26 -0500

    delete all get routes for tank locations because we replaced them with resources: tank_locaionts.

--- config/routes.rb
+++ config/routes.rb
@@ -7,18 +7,6 @@ Rails.application.routes.draw do
 
   get 'comments/destroy'
 
-  # get 'tank_locations/new'
-
-  # get 'tank_locations/create'
-
-  # get 'tank_locations/edit'
-
-  # get 'tank_locations/destroy'
-
-  # get 'tank_locations/show'
-
-  # get 'tank_locations/index'
-
   get 'photos/create'
 
   get 'photos/destroy'
