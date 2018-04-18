c4ca60f4c6f8362ec744749da4a4343390763545
Joshua Alletto <joshalletto@gmail.com>
2018-04-18 09:53:27 -0500

    add paperclip migration to photos.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180415213201) do
+ActiveRecord::Schema.define(version: 20180418144456) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,10 @@ ActiveRecord::Schema.define(version: 20180415213201) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "tank_locations", force: :cascade do |t|
