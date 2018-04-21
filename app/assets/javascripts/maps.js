cd0f57142160f76310bfb9c978801d7ebfd816a0
Joshua Alletto <joshalletto@gmail.com>
2018-04-20 19:54:19 -0500

    choosing a new location removes last marker in Water Tank new

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,29 +1,37 @@
 document.addEventListener('turbolinks:load', function(){
-  createMapWithMarkers()
-  allowMarkerDrop()
-});
-
-
-
-
-function createMapWithMarkers(){
   const locations = JSON.parse(document.querySelector("#map").dataset.locations)
+  var lastMarker
+
   handler = Gmaps.build('Google');
   handler.buildMap({ provider: {maxZoom: 17}, internal: {id: 'map'}}, function(){
     markers = handler.addMarkers(locations);
     handler.bounds.extendWith(markers);
     handler.fitMapToBounds();
   });
-}
 
-function allowMarkerDrop(){
+
   google.maps.event.addListener(handler.getMap(), 'click', function(event) {
-    const lat = event.latLng.lat()
-    const lng = event.latLng.lng()
+    if(lastMarker){
+      lastMarker.setMap(null)
+    }
+    let lat = event.latLng.lat()
+    let lng = event.latLng.lng()
+
+    lastMarker = new google.maps.Marker({
+      map: handler.getMap(),
+      position: new google.maps.LatLng(lat, lng)
+    });
     const formLat = document.querySelector("#tank_location_latitude")
     const formLng = document.querySelector("#tank_location_longitude")
-    handler.addMarker({"lat": lat, "lng": lng})
+    // handler.addMarker({"lat": lat, "lng": lng})
     formLat.value = lat
     formLng.value = lng
   });
-}
+});
+
+
+
+
+
+
+
