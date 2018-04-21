b5b632709058b734ba0bd1d641803048bfaa3505
Joshua Alletto <joshalletto@gmail.com>
2018-04-20 20:02:16 -0500

    refactor lastMarker to take advantage of gmaps4rails addMarker function.

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,6 +1,6 @@
 document.addEventListener('turbolinks:load', function(){
   const locations = JSON.parse(document.querySelector("#map").dataset.locations)
-  var lastMarker
+  let lastMarker
 
   handler = Gmaps.build('Google');
   handler.buildMap({ provider: {maxZoom: 17}, internal: {id: 'map'}}, function(){
@@ -17,13 +17,9 @@ document.addEventListener('turbolinks:load', function(){
     let lat = event.latLng.lat()
     let lng = event.latLng.lng()
 
-    lastMarker = new google.maps.Marker({
-      map: handler.getMap(),
-      position: new google.maps.LatLng(lat, lng)
-    });
+    lastMarker = handler.addMarker({"lat": lat, "lng": lng})
     const formLat = document.querySelector("#tank_location_latitude")
     const formLng = document.querySelector("#tank_location_longitude")
-    // handler.addMarker({"lat": lat, "lng": lng})
     formLat.value = lat
     formLng.value = lng
   });
