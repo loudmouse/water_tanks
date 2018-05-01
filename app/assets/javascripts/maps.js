dee09af643a4df2cc3a8dd6b46d94bae4153ef60
Joshua Alletto <joshalletto@gmail.com>
2018-05-01 13:19:47 -0500

    add conditional to maps.js that only calls allow pin drop if on new water tank page.

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,8 +1,6 @@
 document.addEventListener('turbolinks:load', function(){
 
   const locations = JSON.parse(document.querySelector("#map").dataset.locations)
-  let lastMarker
-  const geocoder = new google.maps.Geocoder
 
   handler = Gmaps.build('Google');
   handler.buildMap({ provider: {maxZoom: 17}, internal: {id: 'map'}}, ()=>{
@@ -11,28 +9,35 @@ document.addEventListener('turbolinks:load', function(){
     handler.fitMapToBounds();
   });
 
-
-  google.maps.event.addListener(handler.getMap(), 'click', (event)=> {
-    if(lastMarker){
-      lastMarker.setMap(null)
-      handler.removeMarker(lastMarker)
-    }
-
-    let location = {
-      "lat": event.latLng.lat(),
-      "lng": event.latLng.lng()
-    }
-    geocoder.geocode({location: location}, (results, status) => {
-      document.querySelector("#tank_location_address").value = results[0].formatted_address
-    })
-
-    lastMarker = handler.addMarker(location)
-
-    document.querySelector("#tank_location_latitude").value  = location.lat
-    document.querySelector("#tank_location_longitude").value = location.lng
-
-  });
+  if(document.querySelector(".tank_locations_new")){
+    allowDropPin()
+  }
 });
+
+function allowDropPin() {
+    let lastMarker
+    const geocoder = new google.maps.Geocoder
+    google.maps.event.addListener(handler.getMap(), 'click', (event)=> {
+      if(lastMarker){
+        lastMarker.setMap(null)
+        handler.removeMarker(lastMarker)
+      }
+
+      let location = {
+        "lat": event.latLng.lat(),
+        "lng": event.latLng.lng()
+      }
+      geocoder.geocode({location: location}, (results, status) => {
+        document.querySelector("#tank_location_address").value = results[0].formatted_address
+      })
+
+      lastMarker = handler.addMarker(location)
+
+      document.querySelector("#tank_location_latitude").value  = location.lat
+      document.querySelector("#tank_location_longitude").value = location.lng
+
+    });
+}
 
 
 
