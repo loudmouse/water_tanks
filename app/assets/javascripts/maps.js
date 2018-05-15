a7733c784788d042654cdfac20dac199e0bbb3bf
Joshua Alletto <joshalletto@gmail.com>
2018-05-15 12:10:06 -0500

    add event listener to overlay to allow pin drop.

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,7 +1,6 @@
 document.addEventListener('turbolinks:load', function(){
 
   const locations = JSON.parse(document.querySelector("#map").dataset.locations)
-  const neighborhoodOverlay = "https://data.cityofchicago.org/api/geospatial/bbvz-uum9?method=export&format=KML" 
   const handler = Gmaps.build('Google');
   
   handler.buildMap({ provider: {maxZoom: 17}, internal: {id: 'map'}}, ()=>{
@@ -10,16 +9,13 @@ document.addEventListener('turbolinks:load', function(){
     handler.fitMapToBounds();
   });
  
-  const neighborhoodOverlayLayer = new google.maps.KmlLayer({
-          url: neighborhoodOverlay,
-          suppressInfoWindows: true,
-          map: handler.getMap()
-        });
+ 
 
-        neighborhoodOverlayLayer.addListener('click', (kmlEvent) => {
-          let text = kmlEvent.featureData.infoWindowHtml;
-          console.log(text.match(/sec_neigh<\/td><td>(.*)\<\/td>/)[1]);
-        });
+        // neighborhoodOverlayLayer.addListener('click', (kmlEvent) => {
+        //   let text = kmlEvent.featureData.infoWindowHtml;
+        //   console.log(kmlEvent)
+        //   console.log(text.match(/sec_neigh<\/td><td>(.*)\<\/td>/)[1]);
+        // });
 
 
  
@@ -31,17 +27,23 @@ document.addEventListener('turbolinks:load', function(){
 function allowDropPin(map) {
   let lastMarker
   const geocoder = new google.maps.Geocoder
+  const neighborhoodOverlay = "https://data.cityofchicago.org/api/geospatial/bbvz-uum9?method=export&format=KML" 
+  const neighborhoodOverlayLayer = new google.maps.KmlLayer({
+          url: neighborhoodOverlay,
+          suppressInfoWindows: true,
+          map: map.getMap()
+        });
 
-  google.maps.event.addListener(map.getMap(), 'click', (event) => {
-
+  neighborhoodOverlayLayer.addListener('click', (kmlEvent) => {
+    
     if(lastMarker){
       lastMarker.setMap(null)
       map.removeMarker(lastMarker)
     }
 
     let location = {
-      "lat": event.latLng.lat(),
-      "lng": event.latLng.lng()
+      "lat": kmlEvent.latLng.lat(),
+      "lng": kmlEvent.latLng.lng()
     }
 
     geocoder.geocode({location: location}, (results, status) => {
@@ -56,7 +58,32 @@ function allowDropPin(map) {
 }
 
 
+// function allowDropPin(map) {
+//   let lastMarker
+//   const geocoder = new google.maps.Geocoder
 
+//   google.maps.event.addListener(map.getMap(), 'click', (event) => {
+
+//     if(lastMarker){
+//       lastMarker.setMap(null)
+//       map.removeMarker(lastMarker)
+//     }
+
+//     let location = {
+//       "lat": event.latLng.lat(),
+//       "lng": event.latLng.lng()
+//     }
+
+//     geocoder.geocode({location: location}, (results, status) => {
+//       document.querySelector("#tank_location_address").value = results[0].formatted_address
+//     })
+
+//     lastMarker = map.addMarker(location)
+
+//     document.querySelector("#tank_location_latitude").value  = location.lat
+//     document.querySelector("#tank_location_longitude").value = location.lng
+//   });
+// }
 
 
 
