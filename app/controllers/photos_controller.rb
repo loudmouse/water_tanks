2e9e0e36e89591c05445b1ee1050aef379701523
Isaac Orme <isorme1@gmail.com>
2018-05-18 14:03:34 -0700

    Adds ability for users to delete their own photos

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -17,6 +17,12 @@ class PhotosController < ApplicationController
   end
 
   def destroy
+    @tank_location = TankLocation.find(params[:id])
+    @photo = @tank_location.photos.first
+    @photo.destroy
+
+    flash[:notice] = "Photo has been removed."
+    redirect_to @tank_location 
   end
 
   def upvote
--- app/controllers/tank_locations_controller.rb
+++ app/controllers/tank_locations_controller.rb
@@ -53,12 +53,14 @@ class TankLocationsController < ApplicationController
         @hash = Gmaps4rails.build_markers(locations) do |tank_location, marker|
           marker.lat tank_location.latitude
           marker.lng tank_location.longitude
-          marker.infowindow ActionController::Base.helpers.image_tag tank_location.photos.first.image.url(:medium)
-          marker.picture({
-                    :url    => "https://image.ibb.co/iQugxS/water_tank_icon.png",
-                    :width  => "32",
-                    :height => "32"
-                   })
+          if !tank_location.photos.empty?
+            marker.infowindow ActionController::Base.helpers.image_tag tank_location.photos.first.image.url(:medium)
+            marker.picture({
+                      :url    => "https://image.ibb.co/iQugxS/water_tank_icon.png",
+                      :width  => "32",
+                      :height => "32"
+                     })
+          end
         end
     end
 
