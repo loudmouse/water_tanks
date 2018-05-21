ff1ee0665026bd4bd10ac50b2deafad8cf32ffe0
Isaac Orme <isorme1@gmail.com>
2018-05-21 16:28:23 -0700

    Adds neighborhood sortable column via ajax

--- app/controllers/tank_locations_controller.rb
+++ app/controllers/tank_locations_controller.rb
@@ -1,8 +1,9 @@
 class TankLocationsController < ApplicationController
   before_action :set_location, only: [:show, :edit, :update, :destroy]
+  helper_method :sort_column, :sort_direction
 
   def index
-    @tank_locations = TankLocation.all
+    @tank_locations = TankLocation.order(sort_column + ' ' + sort_direction)
     make_markers(@tank_locations)
   end
 
@@ -52,17 +53,27 @@ class TankLocationsController < ApplicationController
         @hash = Gmaps4rails.build_markers(locations) do |tank_location, marker|
           marker.lat tank_location.latitude
           marker.lng tank_location.longitude
-          marker.infowindow ActionController::Base.helpers.image_tag tank_location.photos.first.image.url(:medium)
-          marker.picture({
+          if !tank_location.photos.empty?
+            marker.infowindow ActionController::Base.helpers.image_tag tank_location.photos.first.image.url(:medium)
+            marker.picture({
                     :url    => "https://image.ibb.co/iQugxS/water_tank_icon.png",
                     :width  => "32",
                     :height => "32"
                    })
+          end
         end
     end
 
     def set_location
       @tank_location = TankLocation.find(params[:id])
+    end
+
+    def sort_column
+      TankLocation.column_names.include?(params[:sort]) ? params[:sort] : "Address"
+    end
+
+    def sort_direction
+      %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
     end
 
     def tank_location_params
