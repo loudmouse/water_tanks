6efde182979f1237a004af074be91a0eb22f88b0
Isaac Orme <isorme1@gmail.com>
2018-05-30 13:48:16 -0700

    adds lat,long,city,state,zip to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,26 +14,24 @@ def seed_image(file_name)
 end
 
 tank_data1 = [
-  ['1035 West Lake Street', seed_image('chicago-1148-w') ],
-  ['927 West Blackhawk Street', seed_image('grand-161-e2') ],
-  ['2345 South Michigan Avenue', seed_image('grove-2210-s2') ],
-  ['730 West Lake Street', seed_image('halsted-150-n') ],
-  ['809 North Racine', seed_image('img_0351') ],
-  ['509 North Union Avenue', seed_image('img_0717') ]
+  ['1035 W Lake St, Chicago, IL 60607', 41.885291, -87.653566, seed_image('chicago-1148-w') ],
+  ['927 W Blackhawk St, Chicago, IL 60642', 41.907910, -87.652013, seed_image('grand-161-e2') ],
+  ['2345 S Michigan Ave, Chicago, IL 60616', 41.849843, -87.623251, seed_image('grove-2210-s2') ],
+  ['730 W Lake St, Chicago, IL 60661', 41.885973, -87.646894, seed_image('halsted-150-n') ],
+  ['809 N Racine Ave, Chicago, IL 60642', 41.896460, -87.657296, seed_image('img_0351') ]
 ]
 
 tank_data2 = [
-  ['527 South Wells', seed_image('img_1843') ],
-  ['811-813 N Racine', seed_image('img_6587') ],
-  ['161 East Grand Avenue ', seed_image('img_6965') ],
-  ['1500 North Halsted Street', seed_image('img_7534') ],
-  ['1882 South Normal Avenue', seed_image('img_9645') ],
-  ['2210 South Grove Street', seed_image('img_45361') ]
+  ['527 S Wells St, Chicago, IL 60607', 41.875204, -87.633310, seed_image('img_1843') ],
+  ['1500 N Halsted St, Chicago, IL 60642', 41.908752, -87.648479, seed_image('img_7534') ],
+  ['1882 S Normal Ave, Chicago, IL 60616', 41.852308, -87.626711, seed_image('img_9645') ],
+  ['2210 S Grove St, Chicago, IL 60616', 41.852435, -87.639727, seed_image('img_45361') ],
+  ['509 N Union Ave, Chicago, IL 60654', 41.891640, -87.645550, seed_image('img_0717') ]
 ]
 
 tank_data1.each do |tank|
-  tank_location = sample_user1.tank_locations.build(address: tank[0])
-  photo = sample_user1.photos.build(image: tank[1])
+  tank_location = sample_user1.tank_locations.build(address: tank[0], latitude: tank[1] , longitude: tank[2])
+  photo = sample_user1.photos.build(image: tank[3])
   tank_location.photos << photo
   tank_location.photos.first.user_id = sample_user1.id
 
@@ -43,8 +41,8 @@ end
 
 
 tank_data2.each do |tank|
-  tank_location = sample_user2.tank_locations.build(address: tank[0])
-  photo = sample_user2.photos.build(image: tank[1])
+  tank_location = sample_user2.tank_locations.build(address: tank[0], latitude: tank[1] , longitude: tank[2])
+  photo = sample_user2.photos.build(image: tank[3])
   tank_location.photos << photo
   tank_location.photos.first.user_id = sample_user2.id
 
