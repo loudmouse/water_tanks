b0f61634e49b86f861c7b06364ea52d97a6cc67c
Joshua Alletto <joshalletto@gmail.com>
2018-06-05 15:53:06 -0500

    add ability to create and update a user name.

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,11 @@
+class RegistrationsController < Devise::RegistrationsController
+	private 
+
+  def sign_up_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :user_name)
+  end
+
+  def account_update_params
+  	params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :user_name, :current_password)
+  end 
+end 
