ab303458cfd2904a8ed0ff4325a90be33e8fb589
Nolan Neeley <nolanneeley@gmail.com>
2018-06-10 19:29:29 -0500

    style profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,24 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @user_tanks_mapped = @user.tank_locations
+    make_markers(@user_tanks_mapped)
   end
 
   def destroy
   end
+
+  private
+
+    def make_markers(locations)
+        @hash = Gmaps4rails.build_markers(locations) do |tank_location, marker|
+          marker.lat tank_location.latitude
+          marker.lng tank_location.longitude
+          marker.infowindow ActionController::Base.helpers.image_tag tank_location.photos.first.image.url(:medium)
+          marker.picture({
+                    :url    => "https://image.ibb.co/iQugxS/water_tank_icon.png",
+                    :width  => "32",
+                    :height => "32"
+                   })
+        end
+    end
 end
