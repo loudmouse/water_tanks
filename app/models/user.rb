79ca999148b71d61f56f46f09a477747d7a3922f
Nolan Neeley <nolanneeley@gmail.com>
2018-06-11 15:30:38 -0500

    add section for new user to start profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   has_many :tank_locations
   has_many :comments
 
-  has_attached_file :profile_photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/chicago-water-tanks-icon-300x300.png"
+  has_attached_file :profile_photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "profile-photo-placeholder.png"
   validates_attachment_content_type :profile_photo, :content_type => /\Aimage\/.*\Z/
 
   acts_as_voter
