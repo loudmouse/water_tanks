61b1ce0dc9220b5087d4df27f276ef2b8e826d76
Joshua Alletto <joshalletto@gmail.com>
2018-06-15 10:12:52 -0500

    add authentication to photos

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!, only: [:show, :destroy]
+  before_action :authenticate_user!, only: [:show, :destroy, :edit]
   
   def new
   end
