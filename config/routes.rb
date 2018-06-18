49106615eebbc35bb4c60f97e8660296028a0cd1
Joshua Alletto <joshalletto@gmail.com>
2018-06-17 20:37:53 -0500

    add rails admin and fix for conflict with will paginate.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users, :controllers => { registrations: 'registrations' }
   root to: 'tank_locations#index'
   
