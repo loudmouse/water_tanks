0ab6e4cf4088cab5bfb3cdf717c5351f093c06c3
Nolan Neeley <nolanneeley@gmail.com>
2018-08-13 14:01:55 -0500

    included test to check that user has a valid factory

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,10 @@ RSpec.describe User, type: :model do
   it { should have_many(:tank_locations) }
   it { should have_many(:comments) }
   it { should validate_presence_of(:user_name) }
+
+  it "has a valid factory" do
+    user = FactoryBot.create(:user)
+    expect(user).to be_valid
+    expect(user).to be_persisted
+  end
 end
