fb63f0a91c9dd011338ac825dd6414d5f2715e8a
Nolan Neeley <nolanneeley@gmail.com>
2018-08-12 21:06:37 -0500

    wrote matchers for tank location model

--- spec/models/tank_location_spec.rb
+++ spec/models/tank_location_spec.rb
@@ -2,4 +2,8 @@ require 'rails_helper'
 
 RSpec.describe TankLocation, type: :model do
   it { is_expected.to belong_to(:user) }
+  it { should have_many(:photos) }
+  it { should have_many(:comments) }
+  it { should validate_presence_of(:photos) }
+  it { should accept_nested_attributes_for(:photos) }
 end
