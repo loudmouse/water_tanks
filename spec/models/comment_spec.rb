703c5d2da8e3aed4681ac28d7756d87661815d86
Nolan Neeley <nolanneeley@gmail.com>
2018-09-05 20:05:07 -0500

    removed valid comment factory test

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,10 +2,4 @@ require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
    it { should belong_to(:commentable) }
-
-   it "has a valid factory" do
-     comment = FactoryBot.create(:comment)
-     expect(comment).to be_valid
-     expect(comment).to be_persisted
-   end
 end
